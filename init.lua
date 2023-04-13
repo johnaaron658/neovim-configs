48474acb23fbf228c1fab729f88d93d60b6a287c
John Aaron Q. Alcoseba <johnaaron658@gmail.com>
2023-04-14 02:14:43 +0800

    updated neovide settings and registry keys

--- init.lua
+++ init.lua
@@ -5,10 +5,19 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
-vim.o.guifont = "Source Code Pro:h16" 
+vim.o.guifont = "JetBrains Mono:h16" 
+
 vim.g.neovide_scale_factor = 0.75
 vim.g.neovide_padding_top = 0
 vim.g.neovide_padding_bottom = 0
 vim.g.neovide_padding_right = 0
 vim.g.neovide_padding_left = 0
+vim.g.neovide_refresh_rate = 60
+vim.g.neovide_fullscreen = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_vfx_mode = "pixiedust"
+
+
 vim.opt.relativenumber = true
+
